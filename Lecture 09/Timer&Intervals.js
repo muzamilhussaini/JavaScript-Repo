e2fb56b64d7ef83af072c9f62138fd10db768d8c
Muzamilhussaini <karbalaimuzammil50@gmail.com>
2026-08-21 23:50:23 -0700

    Updated the CSS and JS files

--- Lecture 09/Timer&Intervals.js
+++ Lecture 09/Timer&Intervals.js
@@ -1,253 +1,281 @@
-// setTimeout(()=> {
-//     console.log("Hello");
-// }, 2000);
-
-// setInterval(function() {
-//     console.log("Hi");
-// }, 3000);
-
-// const time = setTimeout(()=> {
-//     console.log("Hey");
-// }, 5000);
-// clearTimeout(time);
-
-// const interval = setInterval(function() {
-//     console.log("Running...");
-// }, 4000);
-
-// clearInterval(interval);
-
-// let count = 10;
- 
-// const timer = setInterval(()=> {
-//     console.log(count)
-//     count--;
-//     if (count < 0) {
-//         clearInterval(timer);
-//         console.log("Time's Up!")
-//     }
-// }, 1000);
-
-// const progress = document.querySelector(".progress-bar");
-// const percentage = document.querySelector(".percentage");
-// const h2 = document.querySelector("h2");
-
-// let counter = 0;
-// let seconds = 5;
+setTimeout(()=> {
+    console.log("Hello");
+}, 2000);
 
-// const progressBar = setInterval(()=> {
-//     if (counter < 100) {
-//         counter++;
-//         progress.style.width = `${counter}%`
-//         percentage.textContent = `${counter}%`
-//     } else {
-//         h2.textContent = "Downloaded";
-//         clearInterval(progressBar);
-//     }
-// }, (seconds * 1000) / 100);
+setInterval(function() {
+    console.log("Hi");
+}, 3000);
 
+const time = setTimeout(()=> {
+    console.log("Hey");
+}, 5000);
+clearTimeout(time);
 
-// // Auto-hide alert banner after 3s 
-// const hide = document.querySelector(".hide");
-
-// setTimeout(()=> {
-//     hide.style.display = "none"
-// }, 3000);
+const interval = setInterval(function() {
+    console.log("Running...");
+}, 4000);
 
-// // ============================================================ 
-
-// // Delayed Message
-// // After 3 seconds display:
-// // Hello Muzamil
-
-// setTimeout(()=> {
-//     console.log("Hello Muzamil");
-// }, 3000);
-
-// // Multiple Delays
-// // Print:
-// // HTML
-// // after 1 second
-// // CSS
-// // after 2 seconds
-// // JavaScript
-// // after 3 seconds Using Nested setTimeout 
-
-// setTimeout(() => {
-//     console.log("HTML");
+clearInterval(interval);
 
-//     setTimeout(() => {
-//         console.log("CSS");
+let count = 10;
 
-//         setTimeout(() => {
-//             console.log("JavaScript");
+const timer = setInterval(()=> {
+    console.log(count)
+    count--;
+    if (count < 0) {
+        clearInterval(timer);
+        console.log("Time's Up!")
+    }
+}, 1000);
 
-//         }, 1000);
+const progress = document.querySelector(".progress-bar");
+const percentage = document.querySelector(".percentage");
+const h2 = document.querySelector("h2");
 
-//     }, 1000);
+let counter = 0;
+let seconds = 5;
 
-// }, 1000);
-
-
-// // Question 3: Repeating Message
-// // Print:
-// // Learning JavaScript...
-// // every 2 seconds.
-
-// setInterval(()=>{
-//     console.log("Learning JavaScript..")
-// }, 2000);
-
-// // Question 4: Stop Interval
-// // Create an interval that prints:
-// // Hello
-// // every second.
-// // Stop it after 5 seconds.
-// const stops = setInterval(() => {
-//     console.log("Hello");
-// }, 1000); 
-
-// setTimeout(() => {
-//     clearInterval(stops);
-// }, 5100);
+const progressBar = setInterval(()=> {
+    if (counter < 100) {
+        counter++;
+        progress.style.width = `${counter}%`
+        percentage.textContent = `${counter}%`
+    } else {
+        h2.textContent = "Downloaded";
+        clearInterval(progressBar);
+    }
+}, (seconds * 1000) / 100);
 
-// // ========================================================== 
 
-// // Intermediate Level
+// Auto-hide alert banner after 3s 
+const hide = document.querySelector(".hide");
 
-// // ===========================================================
+setTimeout(()=> {
+    hide.style.display = "none"
+}, 3000);
 
-// // Question 5: Counter
+// ============================================================ 
 
-// // Output:
-// // 1
-// // 2
-// // 3
-// // 4
-// // 5
-// // every second.
-// // After reaching 5, stop automatically.
+// Question 1: Delayed Message
+// After 3 seconds display:
+// Hello Muzamil
+// using setTimeout().
 
-// let C = 0;
+setTimeout(()=> {
+    console.log("Hello Muzamil");
+}, 3000);
 
-// const reach = setInterval(() => {
 
-//     console.log(++C); // Using Pre-increment | Adds 1 to C AND prints it at the same time!
+// Question 2: Multiple Delays
+// Multiple Delays
+// Print:
+// HTML
+// after 1 second
+// CSS
+// after 2 seconds
+// JavaScript
+// after 3 seconds Using Nested setTimeout 
 
-//     if (C >= 5) {
-//         clearInterval(reach);
-//     }
-// }, 1000);
+setTimeout(() => {
+    console.log("HTML");
 
+    setTimeout(() => {
+        console.log("CSS");
 
-// // Question 6: Countdown
-// // Create a countdown
-// // 10
-// // 9
-// // 8
-// // 7
-// // ...
-// // 0
-// // After reaching 0:
+        setTimeout(() => {
+            console.log("JavaScript");
 
-// // Time's Up!
-// // and stop.
+        }, 1000);
 
-// let countdown = 10;
-// let reaching = setInterval(() => {
-//     console.log(countdown);
-//     countdown--;
+    }, 1000);
 
-//     if (countdown < 0) {
-//         console.log("Time's Up!")
-//         clearInterval(reaching);
-//     }
-// }, 1000);
+}, 1000);
 
 
-// // Question 7: Auto Redirect Simulation
-// // After 5 seconds print:
-// // Redirecting...
+// Question 3: Repeating Message
+// Print:
+// Learning JavaScript...
+// every 2 seconds.
 
-// setTimeout(() => {
-//     console.log("Redirecting...")
-// }, 5000);
+setInterval(()=>{
+    console.log("Learning JavaScript..")
+}, 2000);
 
+// Question 4: Stop Interval
+// Create an interval that prints:
+// Hello
+// every second.
+// Stop it after 5 seconds.
+const stops = setInterval(() => {
+    console.log("Hello");
+}, 1000); 
 
-// // Question 8: Loading Effect
-// // Output:
-// // Loading.
-// // Loading..
-// // Loading...
+setTimeout(() => {
+    clearInterval(stops);
+}, 5100);
 
-// // continuously every second.
+// ========================================================== 
 
-// let dots = 1;
-// let stopDots = setInterval(() => {
-//     let dotString = ".".repeat(dots);
-//     console.log(`Loading${dotString}`)
-    
-//     dots++
-//     // dots = 0
-//     if (dots > 3) {
-//         dots = 1
-//     }
-// }, 1000);
+// Intermediate Level
 
+// ===========================================================
 
-// // Challenge Level
-// // Question 9: Digital Clock
+// Question 5: Counter
 
-// // Display current time every second.
-// // Example:
-// // 10:45:01
-// // 10:45:02
-// // 10:45:03
+// Output:
+// 1
+// 2
+// 3
+// 4
+// 5
+// every second.
+// After reaching 5, stop automatically.
 
-// // Hint:
-// // new Date()
+let C = 0;
 
+const reach = setInterval(() => {
 
-// setInterval(() => {
-//     const date = new Date()
+    console.log(++C); // Using Pre-increment | Adds 1 to C AND prints it at the same time!
 
-//     // let hour = date.getHours();
-//     // let changeHour = hour.toString();
-//     // let final = changeHour.padStart(2, "0");
-//     // // console.log(final);
+    if (C >= 5) {
+        clearInterval(reach);
+    }
+}, 1000);
 
 
-//     // let minutes = date.getMinutes();
-//     // let changeMinutes = minutes.toString();
-//     // let ans = changeMinutes.padStart(2, "0");
-//     // // console.log(ans);
+// Question 6: Countdown
+// Create a countdown
+// 10
+// 9
+// 8
+// 7
+// ...
+// 0
+// After reaching 0:
 
+// Time's Up!
+// and stop.
 
-//     // let second = date.getSeconds();
-//     // let changeSecond = second.toString();
-//     // let Final = changeSecond.padStart(2, "0");
-//     // // console.log(Final);
+let countdown = 10;
+let reaching = setInterval(() => {
+    console.log(countdown);
+    countdown--;
 
-//     // console.log(`${final}:${ans}:${Final}`);
-//     // console.log(Totally)
+    if (countdown < 0) {
+        console.log("Time's Up!")
+        clearInterval(reaching);
+    }
+}, 1000);
 
-//     let hour = date.getHours().toString().padStart(2, "0");
-//     let minutes = date.getMinutes().toString().padStart(2, "0");
-//     let second = date.getSeconds().toString().padStart(2, "0");
 
-//     console.log(`${hour}:${minutes}:${second}`)
+// Question 7: Auto Redirect Simulation
+// After 5 seconds print:
+// Redirecting...
 
+setTimeout(() => {
+    console.log("Redirecting...")
+}, 5000);
 
-// }, 1000);
 
+// Question 8: Loading Effect
+// Output:
+// Loading.
+// Loading..
+// Loading...
 
-// // Question 10: Start & Stop Button Logic
-// // Create:
-// // <button>Start</button>
-// // <button>Stop</button>
-// // When Start is clicked:
-// // Hello
-// // prints every second.
-// // When Stop is clicked:
-// // Interval stops.
+// continuously every second.
+
+let dots = 1;
+let stopDots = setInterval(() => {
+    let dotString = ".".repeat(dots);
+    console.log(`Loading${dotString}`)
+
+    dots++
+    // dots = 0
+    if (dots > 3) {
+        dots = 1
+    }
+}, 1000);
+
+
+// Challenge Level
+// Question 9: Digital Clock
+
+// Display current time every second.
+// Example:
+// 10:45:01
+// 10:45:02
+// 10:45:03
+
+// Hint:
+// new Date()
+
+
+setInterval(() => {
+    const date = new Date()
+
+    // let hour = date.getHours();
+    // let changeHour = hour.toString();
+    // let final = changeHour.padStart(2, "0");
+    // // console.log(final);
+
+
+    // let minutes = date.getMinutes();
+    // let changeMinutes = minutes.toString();
+    // let ans = changeMinutes.padStart(2, "0");
+    // // console.log(ans);
+
+
+    // let second = date.getSeconds();
+    // let changeSecond = second.toString();
+    // let Final = changeSecond.padStart(2, "0");
+    // // console.log(Final);
+
+    // console.log(`${final}:${ans}:${Final}`);
+    // console.log(Totally)
+
+    let hour = date.getHours().toString().padStart(2, "0");
+    let minutes = date.getMinutes().toString().padStart(2, "0");
+    let second = date.getSeconds().toString().padStart(2, "0");
+
+    console.log(`${hour}:${minutes}:${second}`)
+
+
+}, 1000);
+
+
+// Question 10: Start & Stop Button Logic
+// Create:
+// <button>Start</button>
+// <button>Stop</button>
+// When Start is clicked:
+// Hello
+// prints every second.
+// When Stop is clicked:
+// Interval stops.
+
+let startBtn = document.querySelector(".startBtn");
+let stopBtn = document.querySelector(".stopBtn");
+let starting = null;
+
+
+startBtn.addEventListener("click", () => {
+    if (starting !== null) {
+        return;
+    }
+
+    starting = setInterval(() => {
+        console.log("Hello");
+    }, 1000);
+    startBtn.classList.add("active-start");
+    stopBtn.classList.remove("active-stop");
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(starting);
+    starting = null;
+    stopBtn.classList.add("active-stop");
+    startBtn.classList.remove("active-start");
+});
+
 
